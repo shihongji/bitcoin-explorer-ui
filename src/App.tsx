d2871b894598bf0086a7f7ed9d5657a2b5eb0ec9
hongji <shihongji21@gmail.com>
2024-06-12 02:25:33 +0800

    refresh btn and automation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import BlockHeight from './BlockHeight';
-import logo from './logo.svg';
+import RefreshButton from './components/RefreshButton';
 import './App.css';
 
 function App() {
